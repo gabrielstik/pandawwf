094e42ba23efff32f5fd7868d6804c427ea31d90
gabrielstik <gabriel.stik@gmail.com>
2017-03-27 16:20:26 +0200

    Braco waves

--- src/js/create-braco.js
+++ src/js/create-braco.js
@@ -1,33 +1,45 @@
 var bracodiv = document.querySelector(".braco");
 
-$(document).ready(function() {
-  spawn();
-});
+function reset() {
+  id = 0;
+  braco = [];
+  bracodiv.innerHTML='';
+}
 
 function spawn() {
   braco = [];
   id = 0;
-  for (var i = 0; i < 10; i+=4) {
+  for (var i = 0; i < 80; i+=4) {
     id++;
-    posY = Math.floor(Math.random()*600);
+    posY = Math.floor(Math.random()*550);
     posX = 0;
-    speed = Math.floor(Math.random()*10);
+    speed = Math.random()*4+3;
     braco[i] = id;
-    braco[i+1] = posY;
+    braco[i+1] = posY-50;
     braco[i+2] = posX;
     braco[i+3] = speed;
-    bracodiv.innerHTML+='<div class="braco-'+id+'">'+braco[i]+','+braco[i+1]+','+braco[i+2]+','+braco[i+3]+'</div>';
+    bracodiv.innerHTML+='<div class="braco-'+id+'"></div>';
     document.querySelector(".braco-"+id).style.top=posY+"px";
   }
   move();
 }
 
 function move() {
-  setInterval(function() {
+  bracoloop = setInterval(function() {
     for (var j = 0; j < braco.length; j+=4) {
       braco[j+2] += braco[j+3];
       document.querySelector(".braco-"+braco[j]).style.right=braco[j+2]+"px";
-      console.log(braco[j+3]);
     }
-  },100);
+  },20);
 }
+
+$(document).ready(function() {
+  // setInterval(function() {
+    try {
+      clearInterval(bracoloop);
+    }
+    catch(e) {}
+    reset();
+    setTimeout('spawn();',100);
+  // },5000);
+});
--- src/js/move.js
+++ src/js/move.js
@@ -3,48 +3,44 @@ $(document).ready(function() {
 })
 
 function keyPressed() {
-	var up = 0; 
-	var left = 0; 
-	var right = 0; 
+	var up = 0;
+	var left = 0;
+	var right = 0;
 	var down = 0;
-	var x = 0; 
+	var x = 0;
 	var y = 0;
+	var speed = 2;
 	document.body.onkeydown = function(e) {
 		if (e.keyCode == 37) { //leftArrow
 			left = 1;
-			console.log("hello");
-			console.log(left);
 		}
 		else if(e.keyCode == 38) { //upArrow
-			up = 1; 
+			up = 1;
 		}
 		else if(e.keyCode == 39) { //rightArrow
-			right = 1; 
+			right = 1;
 		}
 		else if(e.keyCode == 40) { //downArray
-			down = 1; 
+			down = 1;
 		}
 	}
 	document.body.onkeyup = function(e) {
 		if (e.keyCode == 37) { //leftArrow
 			left = 0;
-			console.log(left)
 		}
 		else if(e.keyCode == 38) { //upArrow
-			up = 0; 
+			up = 0;
 		}
 		else if(e.keyCode == 39) { //rightArrow
-			right = 0; 
+			right = 0;
 		}
 		else if(e.keyCode == 40) { //downArray
-			down = 0; 
+			down = 0;
 		}
 	}
 	setInterval(function() {
-		x += (right - left)*15; 
-		y += (down - up)*15;
-		console.log(x);
-		console.log(y);
+		x += (right - left)*speed;
+		y += (down - up)*speed;
 		document.querySelector(".character").style.top = y+"px";
 		document.querySelector(".character").style.left = x+"px";
 	},10);
